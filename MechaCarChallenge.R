4876676ee94fdf155a32f0bd24b4c39840177a38
Arshi Singh <sarsdip@gmail.com>
2021-01-17 14:20:59 -0800

    Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,3 +10,23 @@ lm(mpg~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD
 #Summary 
 
 summary(lm(mpg~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_df))
+
+# Deliverable 2
+
+suspension_coil_df <- read.csv('Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+
+total_summary <- suspension_coil_df %>% summarize(Mean_PSI= mean(PSI), Median_PSI = median(PSI), Variance_PSI = var(PSI), SD_PSI= sd(PSI), .groups='keep')
+
+#Summarize the description stats by Lot
+lot_summary <- suspension_coil_df %>% group_by(Manufacturing_Lot)  %>% summarize(Mean_PSI= mean(PSI), Median_PSI = median(PSI), Variance_PSI = var(PSI), SD_PSI= sd(PSI), .groups='keep')
+
+# Deliverable 3
+
+# t.test to determine if PSI from all manufacturing lots is different from population mean of 1500
+t.test(suspension_coil_df$PSI, mu = 1500)
+
+t.test(subset(suspension_coil_df,Manufacturing_Lot=="Lot1")$PSI,mu=1500)
+
+t.test(subset(suspension_coil_df,Manufacturing_Lot=="Lot2")$PSI,mu=1500)
+
+t.test(subset(suspension_coil_df,Manufacturing_Lot=="Lot3")$PSI,mu=1500)
